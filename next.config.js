392027edec3a0f2cd3c9146adce3ac36208de237
IMNOTPIZZA <48155178+bbeenkh@users.noreply.github.com>
2026-05-28 17:06:14 +0900

    chore: next.config.js 이미지 도메인 와일드카드로 변경 (GNews 다양한 출처 대응)

--- next.config.js
+++ next.config.js
@@ -15,13 +15,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'www.bing.com',
-        pathname: '/.image/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
